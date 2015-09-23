216cccfbebb09a2ef273e81bcd95be80adcc2970
Larry Fantasy <larry.fantasy@gmail.com>
2015-09-23 16:16:41 -0700

    beta

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,1 +1,1 @@
-var Loop=function(){this.frame=this.frame.bind(this),this.lastTime=0};Loop.prototype.start=function(t){this.callback=t,requestAnimationFrame(this.frame)},Loop.prototype.frame=function(t){var i=t-this.lastTime;this.lastTime=t,2e3>i&&this.callback(i),requestAnimationFrame(this.frame)};
+var Loop=function(t){this._fn=t,this.frame=this.frame.bind(this),this.lastTime=0};Loop.prototype.start=function(){requestAnimationFrame(this.frame)},Loop.prototype.frame=function(t){var i=t-this.lastTime;this.lastTime=t,2e3>i&&this._fn(i),requestAnimationFrame(this.frame)};
